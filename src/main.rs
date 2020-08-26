706a39881f08cdc05e3033c6dbf8876db42c27c0
fhancic <fhancic@croz.net>
2020-08-26 11:53:42 +0200

    process input

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
+use std::borrow::BorrowMut;
+use std::collections::{HashMap, HashSet};
 use std::io::{BufRead, stdin};
+use std::iter::Map;
+use std::ops::Deref;
 use std::time::Instant;
+use std::slice::SliceIndex;
+use std::env::temp_dir;
 
-struct Text {
+struct Text<'a> {
     hashes: Vec<i128>,
     queries: Vec<(usize, usize)>,
+    cache: HashMap<(&'a usize, &'a usize), u8>,
 }
 
 fn update_sh(hash: &[u8; 16], sh: &mut [i64; 128]) {
@@ -23,7 +30,6 @@ fn update_sh(hash: &[u8; 16], sh: &mut [i64; 128]) {
 }
 
 fn bin_to_int(sh: &[u8; 128]) -> i128 {
-    println!("{:?}", sh.to_vec());
     let mut result: i128 = 0;
     for i in 0..sh.len() {
         result = (result << 1) | (sh[i] as i128)
@@ -76,12 +82,101 @@ fn process_input(text: &mut Text) {
             }
         }
     }
+    println!("processed {} lines", counter);
+}
+
+fn cut_number(number: &i128, i1: u8, i2: u8) -> i128 {
+    let number = number.clone() >> i1 as i128;
+    let mask = 2i128.pow((i2 - i1) as u32) - 1;
+    return number & mask;
+}
+
+fn process_queries(text: &mut Text, n_bands: u8, band_bits: u8) {
+    let mut candidates: HashMap<usize, HashSet<usize>> = HashMap::new();
+
+    for i in 0..text.hashes.len() {
+        candidates.insert(i, HashSet::new());
+    }
+
+    for band in 0..n_bands {
+        let i1 = band * band_bits;
+        let i2 = i1 + band_bits;
+
+        let mut buckets: HashMap<i128, HashSet<usize>> = HashMap::new();
+
+        for (index, hash) in text.hashes.iter().enumerate() {
+            let band_int = cut_number(hash, i1.into(), i2.into());
+            let mut bucket_content = &mut HashSet::new();
+
+            if buckets.contains_key(&band_int) {
+                bucket_content = buckets.get_mut(&band_int).unwrap();
+
+                for id in bucket_content.iter() {
+                    candidates.get_mut(&index).unwrap().insert(id.clone());
+                    candidates.get_mut(&id).unwrap().insert(index);
+                }
+            }
+            bucket_content.insert(index);
+            let elem = bucket_content.clone();
+            buckets.insert(band_int, elem);
+        }
+    }
+
+    let mut processed: usize = 0;
+    for (i, k) in text.queries.iter() {
+        let hash = text.hashes.get(i.clone()).unwrap();
+        let possible_candidates = candidates.get(i).unwrap();
+
+        let mut counter = 0;
+        for possible in possible_candidates.into_iter() {
+            let possible_hash = text.hashes.get(possible.clone()).unwrap();
+            let ordered = order(i, possible);
+
+            let mut distance: u8;
+            if text.cache.contains_key(&ordered) {
+                distance = text.cache.get(&ordered).unwrap().clone();
+            } else {
+                distance = find_distance(hash, possible_hash);
+            }
+
+            if distance < k.clone() as u8 {
+                counter += 1;
+            }
+        }
+        processed += 1;
+        println!("{}", counter);
+    }
+    println!("processed {} queries", processed)
+}
+
+fn find_distance(hash1: &i128, hash2: &i128) -> u8 {
+    let hash1 = hash1.clone();
+    let hash2 = hash2.clone();
+
+    let mut result = 0;
+    for _ in 0..128 {
+        let x1 = hash1 & 1;
+        let x2 = hash2 & 1;
+        if x1 != x2 {
+            result += 1;
+        }
+    }
+    return result;
+}
+
+fn order<'a>(i1: &'a usize, i2: &'a usize) -> (&'a usize, &'a usize) {
+    if i1 >= i2 {
+        return (i1, i2);
+    }
+    return (i2, i1);
 }
 
 fn main() {
     // println!("simhash: {:x}", simhash("fakultet elektrotehnike i racunarstva"))
-    let mut txt = Text { queries: Vec::new(), hashes: Vec::new() };
+    let mut txt = Text { queries: Vec::new(), hashes: Vec::new(), cache: HashMap::new() };
     let start = Instant::now();
     process_input(&mut txt);
-    println!("process_input done in {:?}", start.elapsed())
+    println!("process_input done in {:?}", start.elapsed());
+    process_queries(&mut txt, 8, 16);
+    println!("all done in {:?}", start.elapsed());
 }
